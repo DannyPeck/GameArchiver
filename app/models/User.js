0a53372f8f626f59ea3f797132e09034253cc8de
Danny Peck <danny.peck@inin.com>
2016-11-24 00:36:52 -0500

    created basic User Model

--- app/models/User.js
+++ app/models/User.js
@@ -2,8 +2,9 @@ var mongodb = require ('@onehilltech/blueprint-mongodb')
   ;
 
 var schema = new mongodb.Schema({
-  firstname: {type: String, required: true, trim: true},
-  lastname: {type: String, required: true, trim: true},
+  email: {type: String, required: true, trim: true},
+  username: {type: String, required: true, trim: true},
+  password: {type: String, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'user';
